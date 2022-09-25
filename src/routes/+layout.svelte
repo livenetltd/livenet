7dacd4ac080147e06366313e4416025fd7c7b471
Adeir Junior <4adeirj@gmail.com>
2022-09-25 06:01:01 +0000

    adicionando assets iniciais e pagina inicial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>LiveNet Ⓒ 2014 - 2022</p>
+	<p>Todos os direitos reservados</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 0;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section id="hero">
+	<h1>
+		O seu provedor de internet em <span>Rolim de Moura</span>
+	</h1>
+
+	<button>Assinar</button>
+
+</section>
+
+<style>
+	#hero {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+		background-color: #19293E;
+		min-height: 200px;
+		padding: 5em 1em 7em;
+	}
+
+	h1 {
+		width: 100%;
+		font-family: Rubik, Arial;
+		font-size: 1.125rem;
+		color: #ffffff;
+		font-weight: bold;
+	}
+</style>
