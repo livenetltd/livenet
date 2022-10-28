6b846a528cb135b92153dd1a903d021cc2124467
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2022-10-28 00:14:08 -0400

    Update +page.svelte

--- src/routes/planos/+page.svelte
+++ src/routes/planos/+page.svelte
@@ -1,6 +1,34 @@
+<script>
+	import { onMount } from 'svelte'
+
+
+onMount(async () => await fetch(`http://Client_Id_e22d6064f6f582610cae5e82373b34ea:Client_Secret_84bfe4c3f44463b69709b274f9a1272257f18c1a@172.31.255.2/api/cliente/`, {
+	mode: 'cors',
+	cache: 'no-cache',
+	headers: {
+		'Authorization': `Bearer`,
+		'Content-Type' : 'application/json',
+	}
+})
+		.then(res => res.json())
+		.then(person => console.log(`Dados: ${person}`))
+		.catch(err => {
+			console.error(err.message)
+		})
+)
+	
+</script>
+
 <svelte:head>
 	<title>Planos</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Os planos de internet da LiveNet foram feitos com o objetivo de atendender as necessidades de cada pessoa" />
 </svelte:head>
 
 <h1>planos</h1>
+
+<style>
+	h1 {
+		margin-top: 2em;
+		text-transform: capitalize;
+	}
+</style>
